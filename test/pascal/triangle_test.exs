d3dad5a48160459d269e06401b26f0c2344e53fa
Alan Peabody <gapeabody@gmail.com>
2013-02-16 13:00:51 -0500

    Alias test subject as Subject.

--- test/pascal/triangle_test.exs
+++ test/pascal/triangle_test.exs
@@ -2,19 +2,20 @@ Code.require_file "../../test_helper.exs", __FILE__
 
 defmodule TriangleTest do
   use ExUnit.Case
+  alias Pascal.Triangle, as: Subject
 
   test "triangles generated correctly" do
-    assert Pascal.Triangle.generate(0) == []
-    assert Pascal.Triangle.generate(1) == [[0,1,0]]
-    assert Pascal.Triangle.generate(2) == [[0,1,0],
-                                           [0,1,1,0]]
-    assert Pascal.Triangle.generate(3) == [[0,1,0],
-                                           [0,1,1,0],
-                                           [0,1,2,1,0]]
-    assert Pascal.Triangle.generate(4) == [[0,1,0],
-                                           [0,1,1,0],
-                                           [0,1,2,1,0],
-                                           [0,1,3,3,1,0]]
+    assert Subject.generate(0) == []
+    assert Subject.generate(1) == [[0,1,0]]
+    assert Subject.generate(2) == [[0,1,0],
+                                   [0,1,1,0]]
+    assert Subject.generate(3) == [[0,1,0],
+                                   [0,1,1,0],
+                                   [0,1,2,1,0]]
+    assert Subject.generate(4) == [[0,1,0],
+                                   [0,1,1,0],
+                                   [0,1,2,1,0],
+                                   [0,1,3,3,1,0]]
   end
 
 end
